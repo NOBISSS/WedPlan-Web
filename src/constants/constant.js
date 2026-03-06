1936d800ab8c174e72a4127c4ff5508905ffdbc7
Parth Chauhan <parthchauhan25125@gmail.com>
2026-03-06 19:57:07 +0530

    URL UPDATED

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,6 +1,6 @@
 //export const BASE_URL="http://localhost:3000/api/v1";
-export const BASE_URL="http://3.110.147.54/api/v1";
-//export const BASE_URL="/api/v1";
+//export const BASE_URL="http://3.110.147.54/api/v1";
+export const BASE_URL="/api/v1";
 export const headers = (token) => ({
     headers: { Authorization: `Bearer ${token}` },
     withCredentials: true,
